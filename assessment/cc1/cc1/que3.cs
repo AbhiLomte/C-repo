675d95559338faf766a734b8ebd69645228803c2
Abhishek Lomte <abhisheklo@infinite.com>
2024-06-24 13:08:07 +0530

    Commit

---
+++ assessment/cc1/cc1/que3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc1
+{
+    class que3
+
+    {
+        static void Main()
+        {
+            int[] input = { 1, 2, 3 };
+            Console.WriteLine("Largest number among{0}, {1},{2} is:{3}", input[0], input[1], input[2], FindLargest(input));
+            Console.Read();
+        }
+
+        static int
+            FindLargest(int[] numbers)
+        {
+            int max = numbers[0];
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            return max;
+
+        }
+    }
+}
