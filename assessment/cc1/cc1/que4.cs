7b8f8d45a353d0efd34a8f4a3d3bf7c3e3363414
Abhishek Lomte <abhisheklo@infinite.com>
2024-06-24 12:34:56 +0530

    Commit

--- assessment/cc1/cc1/que4.cs
+++ assessment/cc1/cc1/que4.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace cc1
 {
     class que4
     {
+        static void Main()
+        {
+            Console.WriteLine("Enter a num ");
+            int a = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(a + "*" + i+"=" +a*i);
+            }
+            Console.Read();
+
+        }
     }
 }
