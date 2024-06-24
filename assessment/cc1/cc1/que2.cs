48a54171ab63fa438c3bf612227056a28fcadad6
Abhishek Lomte <abhisheklo@infinite.com>
2024-06-24 21:38:41 +0530

    commit

---
+++ assessment/cc1/cc1/que2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class program
+{
+    public static void Main()
+    {
+        
+        string[] inputs = { "abcd", "a", "xy" };
+
+        foreach (var input in inputs)
+        {
+            string result = SwapFirstAndLastCharacters(input);
+            Console.WriteLine(result);
+        }
+        Console.Read();
+    }
+
+    static string SwapFirstAndLastCharacters(string str)
+    {
+        
+        if (str.Length < 2)
+        {
+            return str;
+        }
+
+        
+        char[] charArray = str.ToCharArray();
+
+        
+        char temp = charArray[0];
+        charArray[0] = charArray[charArray.Length - 1];
+        charArray[charArray.Length - 1] = temp;
+
+      
+        return new string(charArray);
+    }
+}
