67a268207e13dd212bfe2cebc5e5a260de9b2a65
Abhishek Lomte <abhisheklo@infinite.com>
2024-06-24 12:25:54 +0530

    Commit of cc1

---
+++ assessment/cc1/cc1/que1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace cc1
+{
+    class que1
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("After we remove character at position 1 the string will be :"+RemoveCharacterAtPosition("Python", 1)); 
+          
+
+            Console.Read();
+        }
+
+        static string RemoveCharacterAtPosition(string str, int index)
+        {
+            
+            if (index < 0 || index >= str.Length)
+            {
+                throw new ArgumentOutOfRangeException("index", "Index is out of range.");
+            }
+
+            
+            return str.Remove(index, 1);
+        }
+    }
+}
