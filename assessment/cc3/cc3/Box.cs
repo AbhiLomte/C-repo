6e214aab2c7bc27d5d7e32a36c4d1d4b42d6bf28
Abhishek Lomte <abhisheklo@infinite.com>
2024-07-03 12:43:53 +0530

    commit

---
+++ assessment/cc3/cc3/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Box
+{
+    
+    public double Length { get; set; }
+    public double Breadth { get; set; }
+
+    
+    public Box(double length, double breadth)
+    {
+        Length = length;
+        Breadth = breadth;
+    }
+
+    
+    public static Box AddBoxes(Box box1, Box box2)
+    {
+        double resultLength = box1.Length + box2.Length;
+        double resultBreadth = box1.Breadth + box2.Breadth;
+
+        return new Box(resultLength, resultBreadth);
+    }
+
+    
+    public void DisplayBoxDetails()
+    {
+        Console.WriteLine($"Box Details:");
+        Console.WriteLine($"Length: {Length} units");
+        Console.WriteLine($"Breadth: {Breadth} units");
+    }
+}
---
+++ assessment/cc3/cc3/Cricket.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Cricket
+{
+    private int[] scores;
+
+    
+    public Cricket(int no_of_matches)
+    {
+        scores = new int[no_of_matches];
+    }
+
+   
+    public void Pointscalculation()
+    {
+        int sum = 0;
+
+        
+        for (int i = 0; i < scores.Length; i++)
+        {
+            Console.Write($"Enter score for match {i + 1}: ");
+            scores[i] = Convert.ToInt32(Console.ReadLine());
+            sum += scores[i];
+        }
+
+        
+        double average = (double)sum / scores.Length;
+
+        
+        Console.WriteLine($"Sum of scores: {sum}");
+        Console.WriteLine($"Average of scores: {average:F2}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of matches: ");
+        int numMatches = Convert.ToInt32(Console.ReadLine());
+
+        
+        Cricket cricket = new Cricket(numMatches);
+
+        
+        cricket.Pointscalculation();
+        Console.Read();
+    }
+}
