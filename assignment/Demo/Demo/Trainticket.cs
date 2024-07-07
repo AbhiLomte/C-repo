7b0d1b328e1413abc5c8fb31ec4a91b79a223b5b
Abhishek Lomte <abhisheklo@infinite.com>
2024-07-07 20:25:10 +0530

    commit

---
+++ assignment/Demo/Demo/Trainticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TicketBookingApp
+{
+    class Trainticket
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                TicketBookingLibrary.TicketBooking ticketBooking = new TicketBookingLibrary.TicketBooking();
+                ticketBooking.CalculateConcession(age);
+            }
+            else
+            {
+                Console.WriteLine("Invalid age entered.");
+            }
+
+            Console.ReadLine(); // To keep console window open
+        }
+    }
+}
