5a46870d155f8f0acb99aeb8c051ae4151b3da1a
Abhishek Lomte <abhisheklo@infinite.com>
2024-07-07 20:22:36 +0530

    commit

---
+++ assignment/Assignment6/Assignment6/Program2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program2
+{
+    static void Main()
+    {
+        List<string> words = new List<string> { "mum", "amsterdam", "bloom" };
+
+
+        var query = from word in words
+                    where word.ToLower().StartsWith("a") && word.ToLower().EndsWith("m")
+                    select word;
+
+
+        List<string> resultList = query.ToList();
+
+
+        if (resultList.Count > 0)
+        {
+            Console.WriteLine("Words starting with 'a' and ending with 'm':");
+            foreach (var word in resultList)
+            {
+                Console.WriteLine(word);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No words found starting with 'a' and ending with 'm'.");
+        }
+        Console.Read();
+    }
+}
