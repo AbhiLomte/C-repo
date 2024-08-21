175eecfddca525b63dd47208f42610db041868d9
Abhishek Lomte <abhisheklo@infinite.com>
2024-08-21 16:48:41 +0530

    Web API

---
+++ MVC/Json_post_ajax/Json_post_ajax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Json_post_ajax.MvcApplication" Language="C#" %>
---
+++ Web API/CC10/CC10/Controllers/CountryController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CC10.Controllers
+{   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        // GET: api/country
+        [HttpGet]
+        public IActionResult GetCountries()
+        {
+            // Return a placeholder response
+            return Ok("This is the GET method for all countries.");
+        }
+
+        // GET: api/country/5
+        [HttpGet("{id}")]
+        public IActionResult GetCountry(int id)
+        {
+            // Return a placeholder response
+            return Ok($"This is the GET method for country with ID {id}.");
+        }
+
+        // POST: api/country
+        [HttpPost]
+        public IActionResult CreateCountry([FromBody] string country)
+        {
+            // Return a placeholder response
+            return CreatedAtAction(nameof(GetCountry), new { id = 1 }, country);
+        }
+
+        // PUT: api/country/5
+        [HttpPut("{id}")]
+        public IActionResult UpdateCountry(int id, [FromBody] string country)
+        {
+            // Return a placeholder response
+            return NoContent();
+        }
+
+        // DELETE: api/country/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCountry(int id)
+        {
+            // Return a placeholder response
+            return NoContent();
+        }
+    }
+}
